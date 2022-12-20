265dca58440845633239eef945586859a29fb255
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2022-12-19 23:05:35 -0300

    Mapeamento de entidades

--- src/main/java/com/udemy/udemy/domain/User.java
+++ src/main/java/com/udemy/udemy/domain/User.java
@@ -1,24 +1,42 @@
 package com.udemy.udemy.domain;
 
+import com.udemy.udemy.domain.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name = "user")
 public class User {
 
+    @Id
     private Long id;
+
+    @Column(length = 75, nullable = false)
     private String name;
+
+    @Column(length = 75, nullable = false, unique = true)
     private String email;
+
+    @Column(length = 100, nullable = false)
     private String password;
+
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy= "user")
     private List<Request> requests = new ArrayList<Request>();
+
+    @OneToMany(mappedBy = "user")
     private List<RequestStage> stages = new ArrayList<RequestStage>();
-    
+
 
 }
